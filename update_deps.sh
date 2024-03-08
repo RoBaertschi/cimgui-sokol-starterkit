6e712c5a49cc7672fa7081b30fdecdef07b7efce
Robin <rtmbaertschi007@gmail.com>
2024-03-08 18:19:59 +0100

    Some small changes

--- update_deps.sh
+++ update_deps.sh
@@ -1,20 +1,33 @@
+pushd ../sokol
+git pull
+popd
+
+
 cp ../sokol/sokol_app.h sokol/
 cp ../sokol/sokol_gfx.h sokol/
 cp ../sokol/sokol_log.h sokol/
 cp ../sokol/sokol_glue.h sokol/
 cp ../sokol/util/sokol_imgui.h sokol/
 
-cp ../fips-cimgui/cimgui/cimgui.cpp cimgui/
-cp ../fips-cimgui/cimgui/cimgui.h cimgui/
+pushd ../cimgui
 
-cp ../fips-cimgui/cimgui/imgui/imconfig.h cimgui/imgui
-cp ../fips-cimgui/cimgui/imgui/imgui.cpp cimgui/imgui
-cp ../fips-cimgui/cimgui/imgui/imgui.h cimgui/imgui
-cp ../fips-cimgui/cimgui/imgui/imgui_internal.h cimgui/imgui
-cp ../fips-cimgui/cimgui/imgui/imgui_demo.cpp cimgui/imgui
-cp ../fips-cimgui/cimgui/imgui/imgui_draw.cpp cimgui/imgui
-cp ../fips-cimgui/cimgui/imgui/imgui_widgets.cpp cimgui/imgui
-cp ../fips-cimgui/cimgui/imgui/imgui_tables.cpp cimgui/imgui
-cp ../fips-cimgui/cimgui/imgui/imstb_rectpack.h cimgui/imgui
-cp ../fips-cimgui/cimgui/imgui/imstb_textedit.h cimgui/imgui
-cp ../fips-cimgui/cimgui/imgui/imstb_truetype.h cimgui/imgui
+git fetch --tags
+latestTag=$(git describe --tags "$(git rev-list --tags --max-count=1)")
+echo Updating cimgui to version $latestTag.
+git checkout $latestTag
+popd
+
+cp ../cimgui/cimgui.cpp cimgui/
+cp ../cimgui/cimgui.h cimgui/
+
+cp ../cimgui/imgui/imconfig.h cimgui/imgui
+cp ../cimgui/imgui/imgui.cpp cimgui/imgui
+cp ../cimgui/imgui/imgui.h cimgui/imgui
+cp ../cimgui/imgui/imgui_internal.h cimgui/imgui
+cp ../cimgui/imgui/imgui_demo.cpp cimgui/imgui
+cp ../cimgui/imgui/imgui_draw.cpp cimgui/imgui
+cp ../cimgui/imgui/imgui_widgets.cpp cimgui/imgui
+cp ../cimgui/imgui/imgui_tables.cpp cimgui/imgui
+cp ../cimgui/imgui/imstb_rectpack.h cimgui/imgui
+cp ../cimgui/imgui/imstb_textedit.h cimgui/imgui
+cp ../cimgui/imgui/imstb_truetype.h cimgui/imgui
